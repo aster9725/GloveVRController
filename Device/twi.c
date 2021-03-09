59b7c4f5025e78f49f6750bf3f12bb16db8afa01
심윤석(Youn Seok Shim) <aster9725@users.noreply.github.com>
2021-03-09 22:10:15 +0900

    Update twi.c

--- Device/twi.c
+++ Device/twi.c
@@ -6,18 +6,10 @@
  */ 
 #include "twi.h"
 #include <util/twi.h>
-//#include <string.h>
 
 #define F_SCL 400000UL // SCL frequency
 #define Prescaler 1
 #define TWBR_val ((((F_CPU / F_SCL) / Prescaler) - 16 ) / 2)
-
-//#define SIZE_TWI_BUFFER	64
-//static volatile uint8_t TWIBuffer[SIZE_TWI_BUFFER] = {0, };
-//static volatile uint8_t idxTwi = 0;
-//static volatile uint8_t lenTwiData = 0;
-//static volatile uint8_t isTWIBusy = 0;
-
 
 void TWI_INIT(void)
 {
@@ -25,175 +17,12 @@ void TWI_INIT(void)
 	PORTC = 0x30;
 	TWBR = (uint8_t)TWBR_val;
 	TWSR = 0x00;
-
-
-	//TWCR = _BV(TWEN);	// Enable I2C Interrupt	
-	//isTWIBusy = 0;
-	//idxTwi = 0;
-	//lenTwiData = 0;
 }
 
 void TWI_Stop(void)
 {
 	TWCR = _BV(TWINT) | _BV(TWEN) | _BV(TWSTO);
 }
-
-
-//////////////////////////////////////////////////////////////////////////
-//			Non-Blocking (notwork)
-//////////////////////////////////////////////////////////////////////////
-
-//void TWI_Wait(void)
-//{
-	//while (isTWIBusy);
-//}
-//
-//void TWI_Start(void)
-//{
-	//TWCR = _BV(TWINT) | _BV(TWEN) | _BV(TWIE) | _BV(TWSTA);
-//}
-//
-
-//
-//void TWI_Ack(void)
-//{
-	//TWCR = _BV(TWINT) | _BV(TWEN) | _BV(TWIE) | _BV(TWEA);
-//}
-//
-//void TWI_Nack(void)
-//{
-	//TWCR = _BV(TWINT) | _BV(TWEN) | _BV(TWIE);
-//}
-//
-//void TWI_Transmit(uint8_t data)
-//{
-	//TWDR = data;
-//}
-//
-//void TWI_Receive()
-//{
-	//TWIBuffer[idxTwi] = TWDR;
-//}
-//
-//void TWI_Reply(void)
-//{
-	//if(idxTwi < lenTwiData - 1)
-		//TWI_Ack();
-	//else
-		//TWI_Nack();
-//}
-//
-//void TWI_Done(void)
-//{
-	//isTWIBusy = 0;
-//}
-//
-//uint8_t TWI_WriteReg(uint8_t devaddr, uint8_t regaddr, uint8_t* data, uint8_t length)
-//{
-	//TWI_Wait();
-	//
-	//isTWIBusy = 1;
-	//
-	//TWIBuffer[0] = (devaddr << 1) | TW_WRITE;
-	//TWIBuffer[1] = regaddr;
-	//lenTwiData = length + 2;
-	//idxTwi = 0;
-	//
-	//memcpy(&TWIBuffer[2], data, length);
-	//
-	//TWI_Start();
-	//
-	//return 0;
-//}
-//
-//void TWI_Write(uint8_t address, uint8_t* data, uint8_t length)
-//{
-	//TWI_Wait();
-	//
-	//isTWIBusy = 1;
-	//
-	//TWIBuffer[0] = (address << 1) | TW_WRITE;
-	//lenTwiData = (length + 1);
-	//idxTwi = 0;
-	//
-	//memcpy(&TWIBuffer[1], data, length);
-	//
-	//TWI_Start();
-//}
-//
-//uint8_t TWI_ReadReg(uint8_t devaddr, uint8_t regaddr, uint8_t* data, uint8_t length)
-//{
-	//TWI_Wait();
-	//
-	//isTWIBusy = 1;
-	//
-	//TWIBuffer[0] = (devaddr << 1) | TW_READ;
-	//TWIBuffer[1] = regaddr;
-	//lenTwiData = length + 2;
-	//idxTwi = 0;
-	//
-	//TWI_Start();
-	//
-	//return 0;
-//}
-//
-//void TWI_Read(uint8_t address, uint8_t* data, uint8_t length)
-//{
-	//TWI_Wait();
-	//
-	//isTWIBusy = 1;
-	//
-	//TWIBuffer[0] = (address << 1) | TW_READ;
-	//lenTwiData = length + 1;
-	//idxTwi = 0;
-	//
-	//TWI_Start();
-//}
-//
-//ISR(TWI_vect)
-//{
-	//switch (TW_STATUS) {
-		//case TW_START:
-		//case TW_REP_START:
-		//case TW_MT_SLA_ACK:
-		//case TW_MT_DATA_ACK:
-			//if (idxTwi < lenTwiData) 
-			//{
-				//TWI_Transmit(TWIBuffer[idxTwi++]);
-				//TWI_Nack();
-			//} 
-			//else 
-			//{
-				//TWI_Stop();
-				//TWI_Done();
-			//}
-		//break;
-//
-		//case TW_MR_DATA_ACK:
-		//TWI_Receive();
-		//TWI_Reply();
-		//break;
-//
-		//case TW_MR_SLA_ACK:
-		//TWI_Reply();
-		//break;
-//
-		//case TW_MR_DATA_NACK:
-		//TWI_Receive();
-		//TWI_Stop();
-		//TWI_Done();
-		//break;
-//
-		//case TW_MT_SLA_NACK:
-		//case TW_MR_SLA_NACK:
-		//case TW_MT_DATA_NACK:
-		//default:
-		//TWI_Stop();
-		//TWI_Done();
-		//break;
-	//}
-//}
-
 
 //////////////////////////////////////////////////////////////////////////
 //			Blocking I2C
